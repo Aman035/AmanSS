e0a26aa527d42f5696f351eec39666b960b26bca
Aman Gupta <guptaaman200115@gmail.com>
2020-12-16 00:19:40 +0530

    Updates

---
+++ precache-manifest.61716bf8730c82fd37bf3f70bfc63cdb.js
@@ -0,0 +1,318 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb3d8ba8e61b968ac9224ee189fe606",
+    "url": "/AmanSS/index.html"
+  },
+  {
+    "revision": "b4d0c3487b33577a5a45",
+    "url": "/AmanSS/static/css/2.280cebc5.chunk.css"
+  },
+  {
+    "revision": "f20ca7353fa8716f09a2",
+    "url": "/AmanSS/static/css/main.ae40ec91.chunk.css"
+  },
+  {
+    "revision": "b4d0c3487b33577a5a45",
+    "url": "/AmanSS/static/js/2.48b8984c.chunk.js"
+  },
+  {
+    "revision": "a32cec57b177222bd3e82ae17eed66e2",
+    "url": "/AmanSS/static/js/2.48b8984c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f20ca7353fa8716f09a2",
+    "url": "/AmanSS/static/js/main.6934ffab.chunk.js"
+  },
+  {
+    "revision": "3680e82511d88007f9ba",
+    "url": "/AmanSS/static/js/runtime-main.2b356c3a.js"
+  },
+  {
+    "revision": "f544d090d19fa006fc073aeba7ecbf86",
+    "url": "/AmanSS/static/media/2.f544d090.svg"
+  },
+  {
+    "revision": "9ec71aff11f74a20c1f66221e2124c68",
+    "url": "/AmanSS/static/media/3.9ec71aff.svg"
+  },
+  {
+    "revision": "b3280dcb6c583d403afc0619d185c6a4",
+    "url": "/AmanSS/static/media/4.b3280dcb.svg"
+  },
+  {
+    "revision": "2d163d507354d93da848b572c02f5cb6",
+    "url": "/AmanSS/static/media/5fad6641ed9b5017d16d370d.2d163d50.png"
+  },
+  {
+    "revision": "470ae3d92cdf99dc835efa3915860297",
+    "url": "/AmanSS/static/media/5fad6706ed9b5017d16d370e.470ae3d9.png"
+  },
+  {
+    "revision": "3c71c3fcf33f235f1dd9c3df7516de70",
+    "url": "/AmanSS/static/media/Asset 1.3c71c3fc.svg"
+  },
+  {
+    "revision": "f0439e0fa8459cb86a48bdca277b487b",
+    "url": "/AmanSS/static/media/Boomtv.f0439e0f.png"
+  },
+  {
+    "revision": "691e5036cec3e741e5ef4dfa377032e2",
+    "url": "/AmanSS/static/media/Chainalysis.691e5036.jpg"
+  },
+  {
+    "revision": "436b1bab06a7fb484682cef6f463cf84",
+    "url": "/AmanSS/static/media/Group 120.436b1bab.svg"
+  },
+  {
+    "revision": "3a1bbc052f175701823aed4cbe687d8b",
+    "url": "/AmanSS/static/media/Group 122.3a1bbc05.svg"
+  },
+  {
+    "revision": "abde4201a6e3def15604465ff7a9b630",
+    "url": "/AmanSS/static/media/ai-card.abde4201.svg"
+  },
+  {
+    "revision": "4d2fb5078383c2dea3c97c8ec3bccb64",
+    "url": "/AmanSS/static/media/ai-modal.4d2fb507.svg"
+  },
+  {
+    "revision": "4f2bb5c433957e5aa781aa5fe93f21e1",
+    "url": "/AmanSS/static/media/auditing-card.4f2bb5c4.svg"
+  },
+  {
+    "revision": "fc0d7ae33f0c4a1a11450a7e03e7eb55",
+    "url": "/AmanSS/static/media/auditing-modal.fc0d7ae3.svg"
+  },
+  {
+    "revision": "8ceacb93088ce5c995d09dc93484f8e8",
+    "url": "/AmanSS/static/media/avatar-edit.8ceacb93.png"
+  },
+  {
+    "revision": "2e6a15c7ef3950d8d43ba0100a1ea704",
+    "url": "/AmanSS/static/media/box.2e6a15c7.svg"
+  },
+  {
+    "revision": "8681b221b95dfd7f3d70bb085c5db13f",
+    "url": "/AmanSS/static/media/cards_Account manager.8681b221.svg"
+  },
+  {
+    "revision": "4cc0d6b4e28c3929c810c4272fe9f8f8",
+    "url": "/AmanSS/static/media/cards_Android developer.4cc0d6b4.svg"
+  },
+  {
+    "revision": "aa55cf0b018cb73ac805cf7f9b9364c1",
+    "url": "/AmanSS/static/media/cards_Basic Excel test.aa55cf0b.svg"
+  },
+  {
+    "revision": "06c3f180ece1ad1d573eebf2ed7b2a44",
+    "url": "/AmanSS/static/media/cards_Cold call skills .06c3f180.svg"
+  },
+  {
+    "revision": "9acc53875d74309dc1a6e90db8dba5dd",
+    "url": "/AmanSS/static/media/cards_Community manager.9acc5387.svg"
+  },
+  {
+    "revision": "a808002b881290db3ee5fef41279f5ba",
+    "url": "/AmanSS/static/media/cards_Conternt writer- Entry level.a808002b.svg"
+  },
+  {
+    "revision": "413b3df2be957a03857ea505b0507d38",
+    "url": "/AmanSS/static/media/cards_Data Scientist.413b3df2.svg"
+  },
+  {
+    "revision": "1e07ccbba880bc6a140ff680c9e63313",
+    "url": "/AmanSS/static/media/cards_Data analyst.1e07ccbb.svg"
+  },
+  {
+    "revision": "5a39a4b91ae4bcb7a90b48c28a7975bf",
+    "url": "/AmanSS/static/media/cards_Digital product designer.5a39a4b9.svg"
+  },
+  {
+    "revision": "7f8093e6cbfa50e8ec12b8e3f651250c",
+    "url": "/AmanSS/static/media/cards_Entrepreneurial spirit.7f8093e6.svg"
+  },
+  {
+    "revision": "53689c255367363ee6c6f80a3257c81c",
+    "url": "/AmanSS/static/media/cards_Financial manager.53689c25.svg"
+  },
+  {
+    "revision": "8839f1949a99115cfa7983f4144b1823",
+    "url": "/AmanSS/static/media/cards_Grammar-copywriting.8839f194.svg"
+  },
+  {
+    "revision": "d268f26ec6a495b1d8599832d770041e",
+    "url": "/AmanSS/static/media/cards_Growth maker.d268f26e.svg"
+  },
+  {
+    "revision": "b748dd6a787b9ab2a57b8e2b002f3a01",
+    "url": "/AmanSS/static/media/cards_Junior Editor.b748dd6a.svg"
+  },
+  {
+    "revision": "23069aeba6188382605855f5e66bf600",
+    "url": "/AmanSS/static/media/cards_Leadership and soft skills.23069aeb.svg"
+  },
+  {
+    "revision": "5eb34a9324206f40d7847730cec9d957",
+    "url": "/AmanSS/static/media/cards_Marketing intern.5eb34a93.svg"
+  },
+  {
+    "revision": "ede77afae8c512c05b0bf12a88e3a6d5",
+    "url": "/AmanSS/static/media/cards_Mechanical Engineer.ede77afa.svg"
+  },
+  {
+    "revision": "f42cd80b2b9b662ec384e1fdd5a02b8e",
+    "url": "/AmanSS/static/media/cards_Modern Accounting.f42cd80b.svg"
+  },
+  {
+    "revision": "d71129cbb33d6996f0b1fc0e718c07fb",
+    "url": "/AmanSS/static/media/cards_Operation analyst.d71129cb.svg"
+  },
+  {
+    "revision": "7d51c5c53c287ce033bf5a21e0194375",
+    "url": "/AmanSS/static/media/cards_Proficiency in English.7d51c5c5.svg"
+  },
+  {
+    "revision": "370dc3fc41e9a154e88514681eb2011c",
+    "url": "/AmanSS/static/media/cards_Proficiency in French.370dc3fc.svg"
+  },
+  {
+    "revision": "e3c09143fd920f8516851f6e6bb71310",
+    "url": "/AmanSS/static/media/cards_SEO Web Copywriter.e3c09143.svg"
+  },
+  {
+    "revision": "d22f1ac78064e3612a2f67ec52d3efbc",
+    "url": "/AmanSS/static/media/cards_Sales executive.d22f1ac7.svg"
+  },
+  {
+    "revision": "06e526ba59ef1dd65f116fd5502b65ce",
+    "url": "/AmanSS/static/media/cards_Socoal media marketing.06e526ba.svg"
+  },
+  {
+    "revision": "4eb7a6bdc4fd52175a6b98a79ec7d5e1",
+    "url": "/AmanSS/static/media/cards_Software testing and debugging.4eb7a6bd.svg"
+  },
+  {
+    "revision": "0f3343135c60f62ab151bbcbcc1f6953",
+    "url": "/AmanSS/static/media/cards_Talent acquisition.0f334313.svg"
+  },
+  {
+    "revision": "62cd0db40cb3d0752734dd12140d8d15",
+    "url": "/AmanSS/static/media/cards_UI Designer.62cd0db4.svg"
+  },
+  {
+    "revision": "a8b1a526ca791ba3c6333b1987b34ba7",
+    "url": "/AmanSS/static/media/cards_UX researcher.a8b1a526.svg"
+  },
+  {
+    "revision": "fb677719a51b7f4311197eb90003e81c",
+    "url": "/AmanSS/static/media/cards_ommunication Skills.fb677719.svg"
+  },
+  {
+    "revision": "4b75b9a20d7a20e45b8cff8f482ec976",
+    "url": "/AmanSS/static/media/companies.4b75b9a2.svg"
+  },
+  {
+    "revision": "6bdacf6a46b434ed2aa434a198e4fb1a",
+    "url": "/AmanSS/static/media/ecosystem.6bdacf6a.svg"
+  },
+  {
+    "revision": "92e5aaf6a46b8ac54dfd66851d6d38e1",
+    "url": "/AmanSS/static/media/error.92e5aaf6.svg"
+  },
+  {
+    "revision": "db889dfc4ecec55b7acd3261465a8239",
+    "url": "/AmanSS/static/media/exp-bg.db889dfc.svg"
+  },
+  {
+    "revision": "5051e27314b291de88af39fe6a7d3c76",
+    "url": "/AmanSS/static/media/first-sec-bg.5051e273.svg"
+  },
+  {
+    "revision": "38e1d65f40add773b73d20931605830d",
+    "url": "/AmanSS/static/media/github.38e1d65f.svg"
+  },
+  {
+    "revision": "19a9d2d50249006a3f1fbf528ddd84ce",
+    "url": "/AmanSS/static/media/heart.19a9d2d5.svg"
+  },
+  {
+    "revision": "81ad048ed858673aaca6cc2227076b8a",
+    "url": "/AmanSS/static/media/icons8-google.81ad048e.svg"
+  },
+  {
+    "revision": "970e3faeee14480e40180f52fbc614d9",
+    "url": "/AmanSS/static/media/icons8-home.970e3fae.svg"
+  },
+  {
+    "revision": "d849bc2557228a905cb46a21f1819d90",
+    "url": "/AmanSS/static/media/icons8-linkedin.d849bc25.svg"
+  },
+  {
+    "revision": "414687b54e41d424f391df8ca87517e4",
+    "url": "/AmanSS/static/media/line.414687b5.png"
+  },
+  {
+    "revision": "355bbda19132c2efcd0ce0ba5e8a5250",
+    "url": "/AmanSS/static/media/linkedin.355bbda1.svg"
+  },
+  {
+    "revision": "932449d1e2acbe980b2a9d6007445c6a",
+    "url": "/AmanSS/static/media/mobile-bg-414.932449d1.svg"
+  },
+  {
+    "revision": "0014176bc6fe969835bc9703ba217e5a",
+    "url": "/AmanSS/static/media/mobile-bg-pixel-Xl.0014176b.svg"
+  },
+  {
+    "revision": "89d88fd442393f0a7116eed02b4ffd0c",
+    "url": "/AmanSS/static/media/modal-bottom.89d88fd4.png"
+  },
+  {
+    "revision": "95964aa7ab69db62bec0e162dcba1c88",
+    "url": "/AmanSS/static/media/modal-logo.95964aa7.svg"
+  },
+  {
+    "revision": "c601db7b845e383470f50125a7253bd8",
+    "url": "/AmanSS/static/media/modal-mobile.c601db7b.png"
+  },
+  {
+    "revision": "bf4884ab2fc002c05b4f5c648efe9d9a",
+    "url": "/AmanSS/static/media/negative-card.bf4884ab.svg"
+  },
+  {
+    "revision": "7955e3eceaf12c2071b365a91d7e5bd6",
+    "url": "/AmanSS/static/media/negative-modal.7955e3ec.svg"
+  },
+  {
+    "revision": "955593f2088ef3e996ff842b8e037fcc",
+    "url": "/AmanSS/static/media/orig-1.955593f2.svg"
+  },
+  {
+    "revision": "69b4537c157b05eed977b8e28764d624",
+    "url": "/AmanSS/static/media/personal-website.69b4537c.svg"
+  },
+  {
+    "revision": "627e5aecfe1b9fc96ccb47a4d5b9120f",
+    "url": "/AmanSS/static/media/plan.627e5aec.png"
+  },
+  {
+    "revision": "8dab9e88ab293da668395ed7fcd2e58f",
+    "url": "/AmanSS/static/media/resume-selected.8dab9e88.svg"
+  },
+  {
+    "revision": "389fba8ae87734aab01590129895b69e",
+    "url": "/AmanSS/static/media/resume.389fba8a.svg"
+  },
+  {
+    "revision": "8ac1f1c8f67d15983dd83a1cb6be5e46",
+    "url": "/AmanSS/static/media/voice-card.8ac1f1c8.svg"
+  },
+  {
+    "revision": "ac8a753ee7923837868bf08918b57743",
+    "url": "/AmanSS/static/media/voice-modal.ac8a753e.svg"
+  },
+  {
+    "revision": "ea47a7b60e4652f0ca353fc18e098666",
+    "url": "/AmanSS/static/media/work.ea47a7b6.svg"
+  }
+]);
